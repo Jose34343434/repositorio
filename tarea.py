4effc3e6e252785f04c5bf480629a88d9ca3736f
José Vivallo <firedition@gmail.com>
2025-04-28 21:48:40 -0400

    se agrego funcion de suma

--- tarea.py
+++ tarea.py
@@ -1,1 +1,16 @@
-print("Hola mundo")
+def sumar(a, b):
+    """
+    Suma dos números.
+
+    Args:
+        a (int): Primer número.
+        b (int): Segundo número.
+
+    Returns:
+        int: La suma de a y b.
+    """
+    return a + b
+
+
+
+print(sumar(3,5))
